c7a5639f90f8d3cbcc56fb4174d64f01848115da
sonions123 <o044804m@student.staffs.ac.uk>
2024-07-15 21:00:07 +0100

    Added functionality to search for Skill Category by name
    Added ability to search for skill category by name. Also added extra line to allow search by ID

--- routes/skillCategory.js
+++ routes/skillCategory.js
@@ -2,15 +2,15 @@ const app = require('../app');
 const controller = require('../controllers/skillCategory');
 const express = require('express');
 
-
 const router = express.Router();
 
 router.get('/', controller.getAll);
+router.get('/:id', controller.getById);
+router.get('/category/:category', controller.getByCategory);
 //router.get('/desc/:value', controller.getByDesc);
-//router.get('/:id', controller.getById);
 
 //router.post('/', controller.create);
-//router.delete('/',controller.deleting);
+//router.delete('/', controller.deleting);
 //router.put('/', controller.update);
 
 module.exports = router;
