2d2ab7503b1d55043957fdf6ecee30e6731f4bc1
DanJonesStaffsUni <j013502l@student.staffs.ac.uk>
2024-07-17 19:38:30 +0100

    added getByName
    added getByName to controller and exported.
    Added endpoint onto router for /name/:firstName/:surname

--- routes/employees.js
+++ routes/employees.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/employees');
 
 router.get('/', controller.getAll);
 router.get('/jobrole/:role', controller.getByJobRole); 
+router.get('/name/:firstName/:surname', controller.getByName);
 
 module.exports = router;
